05cb39e8e9dd8fe33e7c1e409d4b83e5afcff0d1
Madness807 <jonathan.terrettaz@gmail.com>
2024-01-18 13:33:04 +0100

    nms

---
+++ ex01/Dog.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joterrett <joterrett@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/18 13:28:01 by joterrett         #+#    #+#             */
+/*   Updated: 2024/01/18 13:32:07 by joterrett        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef [NOM_CLASSE]_HPP
+#define [NOM_CLASSE]_HPP
+
+#include <iostream>
+#include <string>
+// Inclure d'autres bibliothèques nécessaires
+
+class [NomClasse] {
+    public:
+        // Constructeurs
+        [NomClasse]();
+        [NomClasse](const [NomClasse] &source);
+
+        // Destructeur
+        ~[NomClasse]();
+
+        // Opérateur d'affectation
+        [NomClasse] &operator=(const [NomClasse] &source);
+
+        // Autres méthodes publiques
+
+    private:
+        // Attributs privés
+};
+
+#endif /* ![NOM_CLASSE]_HPP */
